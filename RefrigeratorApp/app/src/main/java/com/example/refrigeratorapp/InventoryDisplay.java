47a5299a0b7baed0781c32b5e79e717fdfa6888e
LAPTOP-JQLE5F32\anjum <ammatthew@vcu.edu>
2020-09-18 13:43:33 -0400

    Code to open up inventory page. Not completely done; still need to fix inventory page format

---
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
@@ -0,0 +1,14 @@
+package com.example.refrigeratorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InventoryDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_display);
+    }
+}
