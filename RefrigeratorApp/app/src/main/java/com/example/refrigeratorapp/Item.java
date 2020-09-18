54955d7f5c1e901901c16db4957947777f99d1b9
picardatjp <54410569+picardatjp@users.noreply.github.com>
2020-09-18 00:18:57 -0400

    add items update
    can add items, will make it look nicer later on. for now it just adds and the remove method has been made but not a way to find the entry to be removed.

---
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/Item.java
@@ -0,0 +1,76 @@
+package com.example.refrigeratorapp;
+
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Item {
+    // Item instance variables
+    private String name;
+    private Calendar expDate;
+
+    // Item constructors
+    public Item(){
+        this.name = "food Item";
+        this.expDate = Calendar.getInstance();
+        this.expDate.set(11,11,1111);
+    }
+
+    public Item(String name, Calendar expDate){
+        this.name = name;
+        this.expDate = expDate;
+    }
+
+    public Item(String name, String expDate){
+        this.name = name;
+        int[] dateArr = dateSeparator(expDate);
+        this.expDate = Calendar.getInstance();
+        this.expDate.set(dateArr[2],dateArr[0],dateArr[1]);
+    }
+
+    public Item(String name, int year, int month, int day){
+        this.name = name;
+        this.expDate = Calendar.getInstance();
+        expDate.set(year,month,day);
+    }
+
+    // Separates date String in format mm/dd/yyyy to an integer array
+    public int[] dateSeparator(String date){
+        String[] dateArr = (date.split("/",3));
+        int[] dateArrInt = new int[3];
+        // Parses String date data into integer array
+        for(int i=0;i<dateArr.length;i++){
+            dateArrInt[i] = Integer.parseInt(dateArr[i]);
+        }
+        return dateArrInt;
+    }
+
+    // Checks the validity of the date entered so that dateSeparator() gets the correct format
+    // Returns 0 if invalid and 1 if valid
+    //in progress
+    public boolean dateCheck(Calendar date){
+        return false;
+    }
+
+    // Returns the name of item
+    public String getName(){
+        return this.name;
+    }
+
+    // Returns the expiration date of item
+    public Calendar getExpDate() {
+        return expDate;
+    }
+    // Equals method because I always have a hard time overriding the Object.equals()
+    public boolean isEqual(Item item){
+        return (this.getName().equals(item.getName()) && this.getExpDate().equals(item.getExpDate()));
+    }
+
+    // Describes Item as a String
+    public String toString(){
+        String r = "Name: " + this.getName() + " Exp. Date: " + this.expDate.get(Calendar.MONTH) + "/"
+                + this.expDate.get(Calendar.DAY_OF_MONTH) + "/" + this.expDate.get(Calendar.YEAR);
+        return r;
+    }
+}
---
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/submitScene.java
@@ -0,0 +1,70 @@
+package com.example.refrigeratorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class submitScene extends AppCompatActivity {
+    private ArrayList<Item> itemArray = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_scene);
+
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    // Adds new Item to itemArray and displays success or fail message
+    public void addItem(String name, String expDate){
+        TextView lastItem = findViewById(R.id.textView3);
+       // Item test = new Item("test", expDate);
+       // lastItem.setText(name + " " + expDate);
+        
+        itemArray.add(new Item(name,expDate));
+        lastItem.setText(itemArray.get(itemArray.size()-1).getName() + " added to fridge");
+
+
+    }
+    @SuppressLint("SetTextI18n")
+    // Removes Item from the itemArray
+    // not connected to a button / not used
+    public void removeItem(Item item){
+        TextView lastItem = findViewById(R.id.textView3);
+        for(int i = 0; i < itemArray.size(); i++){
+            if(item.isEqual(itemArray.get(i))){
+                itemArray.remove(i);
+                lastItem.setText(itemArray.get(i).getName() + " was removed");
+                return;
+            }
+        }
+        lastItem.setText("Could not remove item");
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    // Calls addItem()
+    public void submit(View view){
+        EditText foodItemBox = findViewById(R.id.foodItemBox);
+        String name = foodItemBox.getText().toString();
+        EditText foodExpBox = findViewById(R.id.foodExpBox);
+        String expDate = foodExpBox.getText().toString();
+        if(!name.equals("") && !expDate.equals("")){
+            addItem(name, expDate);
+        }
+
+    }
+
+    // Pressing the back button returns user to Main Activity
+    public void goToMain(View view){
+        finish();
+    }
+}
