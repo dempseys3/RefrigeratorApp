0a3d7b7cd829be60662da3eda90ac3cbd01ae7b5
Charlie Dil <40442056+nehadil@users.noreply.github.com>
2020-09-30 13:46:43 -0400

    don't worry about it

--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
@@ -1,14 +1,1 @@
-package com.example.refrigeratorapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class InventoryDisplay extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inventory_display);
-    }
-}
+nah we don't need this
