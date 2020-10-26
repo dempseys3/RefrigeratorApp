9efc53e58dedda0870173bc203466a3fc35706c9
LAPTOP-JQLE5F32\anjum <ammatthew@vcu.edu>
2020-10-26 18:41:50 -0400

    Inventory Recycler View Commits

--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/ItemAdapter.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/ItemAdapter.java
@@ -1,10 +1,16 @@
 package com.example.refrigeratorapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Filterable;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -46,4 +52,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
         return listItems.size();
     }
+
+
 }
