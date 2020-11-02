ea2508e2fd0246b57caa15027c839bd2ccc02141
Sara Dempsey <dempseys3@vcu.edu>
2020-11-02 13:31:34 -0500

    Notification Class

--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/MainActivity.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // code create button that displays inventory
+        displayButton = (Button) findViewById(R.id.displayButton);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+        this.getApplicationContext();
     }
 }
