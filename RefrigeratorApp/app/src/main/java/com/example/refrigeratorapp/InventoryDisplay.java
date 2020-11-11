13c80128608e28dfdb7a872c0b5d56c7d2e30a22
WilliamPickerell <66374031+WilliamPickerell@users.noreply.github.com>
2020-11-11 11:55:43 -0500

    Revert "Merge pull request #21 from dempseys3/Anju_Branch"
    This reverts commit ca0b8aed642ddc2abfa8e0a71fa3f7c2873762a7, reversing
    changes made to fc7763b5d1a52010782c81ac81063c8a33e3cb8d.

--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
@@ -13,10 +13,6 @@ import java.util.ArrayList;
 
 public class InventoryDisplay extends AppCompatActivity {
 
-    private RefrigeratorSQLiteDBHelper refrigeratorDatabase;
-    private ArrayList<Item> allItems = new ArrayList<>();
-    private ItemAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/MainActivity.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.refrigeratorapp;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     Button displayButton;
-
-    //New Stuff for Recycler View
-    private RefrigeratorSQLiteDBHelper mDatabase;
-    //not sure how to go from list to array
-    private List<String> allItems = new ArrayList<>();
-    private ItemAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,26 +29,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //New Stuff for Recycler View
-        ConstraintLayout CLayout = (ConstraintLayout) findViewById(R.id.activity_to_do);
-        RecyclerView contactView = (RecyclerView)findViewById(R.id.product_list);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        contactView.setLayoutManager(linearLayoutManager);
-        contactView.setHasFixedSize(true);
-        mDatabase = new RefrigeratorSQLiteDBHelper(this);
-        allItems = mDatabase.getInventory();
-
-        if(allItems.size() > 0){
-            contactView.setVisibility(View.VISIBLE);
-            mAdapter = new ItemAdapter(this, allItems);
-            contactView.setAdapter(mAdapter);
-
-        }else {
-            contactView.setVisibility(View.GONE);
-            Toast.makeText(this, "There is no contact in the database. Start adding now", Toast.LENGTH_LONG).show();
-        }
 
     }
+
 
     // Starts activity in which user can add to and view the contents of the fridge
     public void goToSubmit(View view){
