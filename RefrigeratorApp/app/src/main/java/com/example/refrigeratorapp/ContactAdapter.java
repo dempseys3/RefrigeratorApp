d81903d3ac08207d932f5ff4e44e7a040a1981d7
LAPTOP-JQLE5F32\anjum <ammatthew@vcu.edu>
2020-11-23 02:28:44 -0500

    Added card view to inventory display

---
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/ContactAdapter.java
@@ -0,0 +1,57 @@
+package com.example.refrigeratorapp;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
+
+    private ArrayList<InventoryItem> contactList;
+
+    public ContactAdapter(ArrayList<InventoryItem> contactList) {
+        this.contactList = contactList;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(ContactViewHolder contactViewHolder, int i) {
+        InventoryItem ci = contactList.get(i);
+        contactViewHolder.vName.setText(ci.getProductName());
+
+
+
+    }
+
+    @Override
+    public ContactViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.card_view_activity, viewGroup, false);
+
+        return new ContactViewHolder(itemView);
+    }
+
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView vName;
+
+
+        public ContactViewHolder(View v) {
+            super(v);
+            vName =  (TextView) v.findViewById(R.id.title);
+
+        }
+    }
+}
--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryDisplay.java
@@ -21,6 +21,7 @@ public class InventoryDisplay extends AppCompatActivity {
     //New Stuff for Recycler View
     private RefrigeratorSQLiteDBHelper mDatabase;
     private ItemAdapter mAdapter;
+    private ContactAdapter ca;
     private ArrayList<InventoryItem> allItems = new ArrayList<>();
 
 
@@ -30,9 +31,42 @@ public class InventoryDisplay extends AppCompatActivity {
         setContentView(R.layout.activity_inventory_display);
 
 
+
+
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        recList.setLayoutManager(llm);
+        recList.setHasFixedSize(true);
+        mDatabase = new RefrigeratorSQLiteDBHelper(this);
+        allItems = mDatabase.getInventory();
+
+        if(allItems.size() > 0){
+            recList.setVisibility(View.VISIBLE);
+            ContactAdapter ca = new ContactAdapter(allItems);
+            //mAdapter = new ItemAdapter(this, allItems);
+            recList.setAdapter(ca);
+
+        }else {
+            recList.setVisibility(View.GONE);
+            Toast.makeText(this, "There is no contact in the database. Start adding now", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
+
+
+
+
+
         //New Stuff for Recycler View
-        ConstraintLayout CLayout = (ConstraintLayout) findViewById(R.id.activity_to_do);
-        RecyclerView contactView = (RecyclerView)findViewById(R.id.product_list);
+        //ConstraintLayout CLayout = (ConstraintLayout) findViewById(R.id.activity_to_do);
+        /*RecyclerView contactView = (RecyclerView)findViewById(R.id.product_list);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         contactView.setLayoutManager(linearLayoutManager);
         contactView.setHasFixedSize(true);
@@ -47,7 +81,7 @@ public class InventoryDisplay extends AppCompatActivity {
         }else {
             contactView.setVisibility(View.GONE);
             Toast.makeText(this, "There is no contact in the database. Start adding now", Toast.LENGTH_LONG).show();
-        }
+        }*/
 
 
 
