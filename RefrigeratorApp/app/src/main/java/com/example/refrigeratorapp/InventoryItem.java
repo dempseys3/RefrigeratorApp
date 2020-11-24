a498cab87a83071e67f8f04017852af35330c92c
WilliamPickerell <66374031+WilliamPickerell@users.noreply.github.com>
2020-11-24 14:48:08 -0500

    Added database options to lower count and set up database for shoping list

--- RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryItem.java
+++ RefrigeratorApp/app/src/main/java/com/example/refrigeratorapp/InventoryItem.java
@@ -1,11 +1,19 @@
 package com.example.refrigeratorapp;
 
 public class InventoryItem {
+    private int id;
     private String productName;
     private int count;
     private String expiryDate;
 
     public InventoryItem(String productName, int count, String expiryDate) {
+        this.productName = productName;
+        this.expiryDate = expiryDate;
+        this.count = count;
+    }
+
+    public InventoryItem(int id, String productName, int count, String expiryDate) {
+        this.id = id;
         this.productName = productName;
         this.expiryDate = expiryDate;
         this.count = count;
@@ -24,8 +32,14 @@ public class InventoryItem {
         return expiryDate;
     }
 
+    int getId() {return id;}
+
     Boolean isEqual(InventoryItem item){
-        return this.productName.equals(item.productName) && this.expiryDate.equals(item.expiryDate);
+        if(this.productName == item.productName && this.expiryDate == item.expiryDate){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
